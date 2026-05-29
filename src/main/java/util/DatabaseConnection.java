0d7f0e42511966c58285f58c212fda0ecc47c3d8
Wolfanyy <gribanova.anna98@mail.ru>
2026-05-29 22:24:59 +0400

    feat: add JDBC database connection utility

---
+++ src/main/java/util/DatabaseConnection.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/users_db";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USER, PASSWORD);
+    }
+}
