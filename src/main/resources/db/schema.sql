50d9eba025ca6de4ce25ce45a4c0ea65b9e19737
Wolfanyy <gribanova.anna98@mail.ru>
2026-06-04 14:21:54 +0400

    feat: add database schema script

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users
+(
+    id        BIGSERIAL PRIMARY KEY,
+    name      VARCHAR(100)        NOT NULL,
+    last_name VARCHAR(100)        NOT NULL,
+    email     VARCHAR(255) UNIQUE NOT NULL,
+    age       INTEGER             NOT NULL
+);
