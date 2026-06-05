5feed569bf2bfbc9cb1316110dc059a5128fe5cd
Wolfanyy <gribanova.anna98@mail.ru>
2026-06-05 20:41:04 +0400

    feat: add UserService interface

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,18 @@
+package service;
+
+import model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    User save(User user);
+
+    Optional<User> findById(Long id);
+
+    List<User> findAll();
+
+    boolean update(User user);
+
+    boolean deleteById(Long id);
+}
