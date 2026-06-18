43180973082bc37f969fbd662da29887394a91fc
Wolfanyy <gribanova.anna98@mail.ru>
2026-06-18 12:47:56 +0400

    feat: add UserListServlet

---
+++ src/main/java/servlet/UserListServlet.java
@@ -0,0 +1,35 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.User;
+import service.UserService;
+import util.AppContext;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/users")
+public class UserListServlet extends HttpServlet {
+
+    private UserService userService;
+
+    public void init() {
+        userService = AppContext.getUserService();
+    }
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        List<User> users = userService.findAll();
+
+        request.setAttribute("users", users);
+
+        request.getRequestDispatcher("/WEB-INF/jsp/users.jsp")
+                .forward(request, response);
+    }
+}
